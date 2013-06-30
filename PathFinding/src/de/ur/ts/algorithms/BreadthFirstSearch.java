a4a2bc0ced23c4d6f6aeff7ae19a6523112bf629
thhesp <Thomas_spr@yahoo.de>
2013-06-30 02:59:15 +0200

    Changed names to english versions

---
+++ PathFinding/src/de/ur/ts/algorithms/BreadthFirstSearch.java
@@ -0,0 +1,16 @@
+package de.ur.ts.algorithms;
+
+import de.ur.ts.map.Map;
+
+public class BreadthFirstSearch extends Algorithm {
+	
+	public BreadthFirstSearch(Map map){
+		super(map);
+	}
+
+	@Override
+	protected void work() {
+
+	}
+
+}
---
+++ PathFinding/src/de/ur/ts/algorithms/DepthFirstSearch.java
@@ -0,0 +1,44 @@
+package de.ur.ts.algorithms;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import de.ur.ts.map.Field;
+import de.ur.ts.map.Map;
+
+public class DepthFirstSearch extends Algorithm {
+	
+	public DepthFirstSearch(Map map) {
+		super(map);
+	}
+
+	@Override
+	protected void work() {
+		if(currentField.equals(goal)){
+			finished = true;
+			return;
+		}
+		boolean fieldFound = false;
+		ArrayList<Field> neighbors = currentField.getNeighbors();
+		if(neighbors.isEmpty()){
+			stepBack();
+			return;
+		}
+		Iterator it = neighbors.iterator();
+		while(it.hasNext()){
+			Field f = ((Field) it.next());
+			if(!f.isInUse() && f.isEmpty()){
+				setCurrentField(f);
+				fieldFound = true;
+				return;
+			}
+		}
+		
+		if(!fieldFound){
+			stepBack();
+		}
+	}
+
+
+
+}
